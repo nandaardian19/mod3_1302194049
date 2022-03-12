c1f6c6cbeb484dea6f86a3094efe3a8e55e4c702
nandaardian19 <93126096+nandaardian19@users.noreply.github.com>
2022-03-12 14:20:34 +0700

    Add files via upload

---
+++ mod3no5_1302194049/mod3no5_1302194049/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod3no5_1302194049
+{
+    public class PosisiKarakterGame {
+        private String state = "Berdiri";
+        public void posisi (String perintah)
+        {
+            perintah = perintah.ToLowerCase();
+            switch (perintah) {
+                case "berdiri":
+                    this.state = "posisi standby";
+                    break;
+                case "Tengkurap":
+                    this.state = "posisi istirahat";
+                    break ;
+
+        }
+
+    }
+        public String getState ()
+        {
+            return this.state;
+        }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
